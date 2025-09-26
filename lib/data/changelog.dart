116fc2439093089d176bba1628461064b81bf085
Archit Kumar <architxkumar@proton.me>
2025-09-26 14:02:06 +0530

    feat: add changelog data and model

---
+++ lib/data/changelog.dart
@@ -0,0 +1,15 @@
+const String changelogJsonString = '''
+[
+  {"date": "August 1, 2024", "change": "Changelog page to help you stay in the loop"},
+  {"date": "August 8, 2024", "change": "Dark mode support rolled out"},
+  {"date": "August 10, 2024", "change": "Redis roadmap"},
+  {"date": "August 15, 2024", "change": "Backend Project Ideas"},
+  {"date": "August 18, 2024", "change": "Leaderboard for top contributors added"},
+  {"date": "August 22, 2024", "change": "Submit your project solution and get feedback"},
+  {"date": "August 25, 2024", "change": "Git and GitHub Roadmap"},
+  {"date": "August 28, 2024", "change": "Build your learning habits with learning streaks"},
+  {"date": "September 3, 2024", "change": "Announcing Projects on Frontend Roadmap"},
+  {"date": "September 12, 2024", "change": "Profile pages: Users can now add bio and tech stack"},
+  {"date": "September 20, 2024", "change": "Mobile app beta launched for select users"}
+]
+''';
---
+++ lib/model/changelog.dart
@@ -0,0 +1,6 @@
+class Changelog {
+  final String date;
+  final String change;
+
+  Changelog({required this.date, required this.change});
+}
